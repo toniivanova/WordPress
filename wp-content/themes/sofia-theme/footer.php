fbfe10b685a3fd7681b79ad7cef23808d8e6e498
toniivanova <itivanovatoni@gmail.com>
2020-01-13 16:58:19 +0200

    update footer

--- wp-content/themes/sofia-theme/footer.php
+++ wp-content/themes/sofia-theme/footer.php
@@ -1,26 +1,13 @@
-<footer class="container">
-<div>
-    <nav>
-        <h5>Menu</h5>
-        <ul>
-            <li><a href="about-me" title="About me">About me</a></li>
-            <li><a href="news">News</a></li>
-            <li><a href="horoscope">Horoscope</a></li>
-            <li><a href="social-network">Network</a></li>
-            <li><a href="../event">Events</a></li>
-        </ul>
-    </nav>
-    
+<footer class="footer">
     <nav>
         <h5>Contact</h5>
         <ul>
-        <li>Company Name,</li>
-        <li>bul. Carigradsko shose 1,</li>
-        <li>1000 Sofia,</li>
-        <li>0888123456</li> 
+            <li>Company Name,</li>
+            <li>bul. Carigradsko shose 1,</li>
+            <li>1000 Sofia,</li>
+            <li>0888123456</li> 
         </ul>
     </nav>    
-    
     <p><a href="#">Back to top</a></p>
 </footer>    
 
